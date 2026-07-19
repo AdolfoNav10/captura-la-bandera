e270fd939e2c76a2c9e990fd333cea899a9d352f
Javier Navarro <adolfonav68@gmail.com~>
2026-07-19 15:23:10 -0600

    cliente y servidor intercambian join y welcome

--- src/cliente/redCliente.py
+++ src/cliente/redCliente.py
@@ -1,4 +1,5 @@
 import socket
+from src.comun import protocolo as p
 
 PUERTO_JUEGO = 8889
 
@@ -7,6 +8,21 @@ def conectar_a_servidor(ip_servidor):
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.connect((ip_servidor, PUERTO_JUEGO))
     print("Conectado al servidor")
+
+    mensaje_join = {
+        "type": "join",
+        "name": "Gaby",
+    }
+    p.enviar(cliente, mensaje_join)
+    print("Se envio join al servidor")
+
+    lector = p.LectorMensajes()
+    datos_recibidos = cliente.recv(1024)
+    mensajes = lector.agregar_bytes(datos_recibidos)
+
+    for mensaje in mensajes:
+        print("Respuesta del servidor:", mensaje)
+
     return cliente
 
 
--- src/servidor/redServidor.py
+++ src/servidor/redServidor.py
@@ -1,4 +1,6 @@
 import socket
+from src.comun import protocolo as p
+from src.comun.constantes import CONFIG_DEFAULT
 
 PUERTO_JUEGO = 8889
 
@@ -11,6 +13,21 @@ def iniciar_servidor():
 
     conexion, direccion = servidor.accept()
     print("Se conecto un cliente desde", direccion)
+
+    lector = p.LectorMensajes()
+    datos_recibidos = conexion.recv(1024)
+    mensajes = lector.agregar_bytes(datos_recibidos)
+
+    for mensaje in mensajes:
+        print("Mensaje recibido:", mensaje)
+        if mensaje["type"] == "join":
+            respuesta = {
+                "type": "welcome",
+                "player_id": "p1",
+                "config": CONFIG_DEFAULT,
+            }
+            p.enviar(conexion, respuesta)
+            print("Se envio welcome al cliente")
 
 
 if __name__ == "__main__":
